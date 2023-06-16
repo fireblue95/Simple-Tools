116453c8bf02198f0901ea2fdd0b9299bcc357d0
fireblue95 <128771832+fireblue95@users.noreply.github.com>
2023-06-16 22:44:24 +0800

    Version 1.0.0 20230616 Updated.

--- install_venv/install_venv.sh
+++ install_venv/install_venv.sh
@@ -1,3 +1,6 @@
+# Need to use `source install_venv.sh` to run this file.
+# Also can use bash to run but the user needs to use `source ~/.bashrc` after installation is finished.
+
 sudo apt-get install -y python3-dev python3-pip python3-testresources
 sudo pip install virtualenv virtualenvwrapper
 echo "installing virtualenvironment"
@@ -6,7 +9,6 @@ echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python$(python3 -V | cut -d " " -
 echo "export WORKON_HOME=$HOME/pyenvs" >> ~/.bashrc
 echo "export PROJECT_HOME=$HOME/projects" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
-
 
 source ~/.bashrc
 
